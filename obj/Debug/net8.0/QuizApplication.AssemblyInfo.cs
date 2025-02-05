a90a79202e74ba14a1ba2733a79af82a5c421d02
Dhairya3391 <dhairyaadroja3391@gmail.com>
2025-02-05 19:03:04 +0530

    Add initial project files

--- obj/Debug/net8.0/QuizApplication.AssemblyInfo.cs
+++ obj/Debug/net8.0/QuizApplication.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QuizApplication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf12e1d93ffbb32bdaef09fc4097f4a42e19f1df")]
 [assembly: System.Reflection.AssemblyProductAttribute("QuizApplication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QuizApplication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
