7f4a4ea59bfa24ee0426f0933e09007b5f1adade
Harpalsinh Sindhav <88263558+harpalll@users.noreply.github.com>
2025-03-02 22:17:28 +0530

    Committing changes before switching branches

--- Controllers/QuizWiseQuestionController.cs
+++ Controllers/QuizWiseQuestionController.cs
@@ -1,37 +1,61 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Data;
+using Microsoft.AspNetCore.Mvc;
 using QuizApplication.DbConfigruation;
 using QuizApplication.Models;
+using QuizApplication.QuizWiseQuestionCRUDCRUD;
 
 namespace QuizApplication.Controllers
 {
     public class QuizWiseQuestionController : Controller
     {
         private readonly DbConfiguration _dbConfiguration;
-        public QuizWiseQuestionController(DbConfiguration dbConfiguration)
+        private readonly QuizWiseQuestionCRUD _quizWiseQuestion;
+        public QuizWiseQuestionController(DbConfiguration dbConfiguration, QuizWiseQuestionCRUD quizWiseQuestion)
         {
             this._dbConfiguration = dbConfiguration;
-
+            this._quizWiseQuestion = quizWiseQuestion;
         }
+
+        public IActionResult QuizWiseQuestionList()
+        {
+            return View(_dbConfiguration.GetAllData("PR_QuizQuestionsWise_SelectAll"));
+        }
+
         public IActionResult QuizWiseQuestionForm()
         {
             return View();
         }
-        public IActionResult QuizWiseQuestionList()
-        {
-            return View(_dbConfiguration.GetAllData("PR_QuizQuestionsWise_SelectAll"));
-
-        }
 
         [HttpPost]
-        public ActionResult AddQuizWiseQuestion(QuizWiseQuestionModel model)
+        public ActionResult AddQuizWiseQuestionEntry(QuizWiseQuestionModel model)
         {
             if (ModelState.IsValid)
             {
-
                 return RedirectToAction("QuizWiseQuestionList");
             }
 
             return View("QuizWiseQuestionForm", model);
         }
+
+        public IActionResult QuizWiseQuestionDetails(int quizId, string quizName,int totalQuestions)
+        {
+            return RedirectToAction("QuizWiseQuestionDetailsAction", "QuizWiseQuestion",
+        new { quizId = quizId, quizName = quizName, totalQuestions = totalQuestions });
+        }
+
+        public IActionResult QuizWiseQuestionDetailsAction(int quizId, string quizName, int totalQuestions)
+        {
+            ViewBag.QuizName = quizName;
+            ViewBag.TotalQuestions = totalQuestions;
+            return View("QuizWiseQuestionDetails", _quizWiseQuestion.GetQuizWiseQuestionDetails(quizId));
+        }
+
+        public IActionResult AddQuizWiseQuestions(string quizName, int totalQuestions) {
+            ViewBag.QuizName = quizName;
+            ViewBag.TotalQuestions = totalQuestions;
+            return View(_dbConfiguration.GetAllData("PR_Question_SelectAll"));
+            //return View();
+        }
+
     }
 }
--- QuizWiseQuestionCRUD/QuizWiseQuestionCRUD.cs
+++ QuizWiseQuestionCRUD/QuizWiseQuestionCRUD.cs
@@ -1,5 +1,6 @@
 ﻿using System.Data;
 using System.Data.SqlClient;
+using System.Xml.Linq;
 using QuizApplication.Models;
 
 namespace QuizApplication.QuizWiseQuestionCRUDCRUD
@@ -13,22 +14,72 @@ namespace QuizApplication.QuizWiseQuestionCRUDCRUD
             configuration = _configuration;
         }
 
-        public bool AddQuiz(QuizModel model)
+        //public bool AddQuiz(QuizWiseQuestionModel model)
+        //{
+        //    string connectionString = configuration.GetConnectionString("ConnectionString");
+
+        //    SqlConnection connection = new SqlConnection(connectionString);
+        //    connection.Open();
+        //    SqlCommand command = new SqlCommand("PR_Quiz_Insert", connection);
+        //    command.CommandType = CommandType.StoredProcedure;
+
+        //    command.Parameters.AddWithValue("@QuizName", model.QuizName);
+        //    command.Parameters.AddWithValue("@TotalQuestions", model.TotalQuestions);
+        //    command.Parameters.AddWithValue("@QuizDate", model.QuizDate);
+        //    command.Parameters.AddWithValue("@UserID", 1);
+        //    int result = command.ExecuteNonQuery();
+        //    return result > 0;
+        //}
+
+        //public DataTable GetQuizWiseQuestionDetails(QuizWiseQuestionModel model)
+        //{
+        //    string connectionString = configuration.GetConnectionString("ConnectionString");
+
+        //    using (SqlConnection connection = new SqlConnection(connectionString))
+        //    {
+        //        connection.Open();
+
+        //        using (SqlCommand command = connection.CreateCommand())
+        //        {
+        //            command.CommandType = CommandType.StoredProcedure;
+        //            command.CommandText = "PR_QuizQuestionsWise_Details";
+
+        //            using (SqlDataReader reader = command.ExecuteReader())
+        //            {
+        //                DataTable table = new DataTable();
+        //                table.Load(reader);
+        //                return table;
+        //            }
+        //        }
+        //    }
+        //}
+
+        public DataTable GetQuizWiseQuestionDetails(int quizId)
         {
             string connectionString = configuration.GetConnectionString("ConnectionString");
 
-            SqlConnection connection = new SqlConnection(connectionString);
-            connection.Open();
-            SqlCommand command = new SqlCommand("PR_Quiz_Insert", connection);
-            command.CommandType = CommandType.StoredProcedure;
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
 
-            command.Parameters.AddWithValue("@QuizName", model.QuizName);
-            command.Parameters.AddWithValue("@TotalQuestions", model.TotalQuestions);
-            command.Parameters.AddWithValue("@QuizDate", model.QuizDate);
-            command.Parameters.AddWithValue("@UserID", 1);
-            int result = command.ExecuteNonQuery();
-            return result > 0;
+                using (SqlCommand command = connection.CreateCommand())
+                {
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.CommandText = "PR_QuizQuestionsWise_Details";
 
+                    // ✅ Pass quizId as a parameter to the stored procedure
+                    command.Parameters.AddWithValue("@QuizID", quizId);
+
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        DataTable table = new DataTable();
+                        table.Load(reader);
+                        return table;
+                    }
+                }
+            }
         }
+
+
     }
 }
