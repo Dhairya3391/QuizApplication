6679ab8a9a3a0568c0eb0e27569a9d396404b96c
Harpalsinh Sindhav <88263558+harpalll@users.noreply.github.com>
2025-03-02 23:43:14 +0530

    QWQ fetch and QWQ question add done.

--- Controllers/QuizWiseQuestionController.cs
+++ Controllers/QuizWiseQuestionController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using QuizApplication.DbConfigruation;
 using QuizApplication.Models;
+using QuizApplication.QuestionCRUD;
 using QuizApplication.QuizWiseQuestionCRUDCRUD;
 
 namespace QuizApplication.Controllers
@@ -51,10 +52,76 @@ namespace QuizApplication.Controllers
         }
 
         public IActionResult AddQuizWiseQuestions(string quizName, int totalQuestions) {
+            //ViewBag.QuizName = quizName;
+            //ViewBag.TotalQuestions = totalQuestions;
+            //return View(_dbConfiguration.GetAllData("PR_Question_SelectAll"));
+            //return View();
+            int quizId = _quizWiseQuestion.GetQuizIdByName(quizName);
+            ViewBag.QuizId = quizId;
             ViewBag.QuizName = quizName;
             ViewBag.TotalQuestions = totalQuestions;
-            return View(_dbConfiguration.GetAllData("PR_Question_SelectAll"));
-            //return View();
+
+            // Fetch already added questions
+            List<int> addedQuestions = _quizWiseQuestion.GetAddedQuestionIds(quizId);
+            ViewBag.AddedQuestions = addedQuestions;
+
+            // Fetch all available questions
+            DataTable allQuestions = _dbConfiguration.GetAllData("PR_Question_SelectAll");
+
+            return View(allQuestions);
+        }
+
+        //[HttpPost]
+        //public IActionResult SaveQuizQuestions(int quizId, List<int> selectedQuestions)
+        //{
+        //    if (selectedQuestions == null || selectedQuestions.Count == 0)
+        //    {
+        //        TempData["ErrorMessage"] = "Please select at least one question.";
+        //        return RedirectToAction("QuizWiseQuestionDetails", new { quizId = quizId });
+        //    }
+
+        //    try
+        //    {
+        //        _quizWiseQuestion.SaveSelectedQuestions(quizId, selectedQuestions);
+        //        TempData["SuccessMessage"] = "Questions added successfully!";
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        TempData["ErrorMessage"] = "Error adding questions: " + ex.Message;
+        //    }
+
+        //    return RedirectToAction("QuizWiseQuestionList");
+        //}
+
+
+        [HttpPost]
+        public IActionResult SaveQuizQuestions(int quizId, List<int> selectedQuestions)
+        {
+            int userId = 1; // Assume logged-in user ID
+
+            foreach (var questionId in selectedQuestions)
+            {
+                _quizWiseQuestion.AddQuizWiseQuestion(quizId, questionId, userId);
+            }
+
+            return RedirectToAction("QuizWiseQuestionList");
+        }
+
+        [HttpGet]
+        public ActionResult DeleteQuizWiseQuestion(int quizWiseQuestionID)
+        {
+            bool isDeleted = _quizWiseQuestion.DeleteQuizWiseQuestion(quizWiseQuestionID);
+
+            if (isDeleted)
+            {
+                TempData["SuccessMessage"] = "Question deleted successfully.";
+            }
+            else
+            {
+                TempData["ErrorMessage"] = "Error while deleting question.";
+            }
+
+            return RedirectToAction("QuizWiseQuestionList");
         }
 
     }
--- QuizWiseQuestionCRUD/QuizWiseQuestionCRUD.cs
+++ QuizWiseQuestionCRUD/QuizWiseQuestionCRUD.cs
@@ -80,6 +80,106 @@ namespace QuizApplication.QuizWiseQuestionCRUDCRUD
             }
         }
 
+        public void SaveSelectedQuestions(int quizId, List<int> selectedQuestions)
+        {
+            string connectionString = configuration.GetConnectionString("ConnectionString");
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                foreach (int questionId in selectedQuestions)
+                {
+                    using (SqlCommand command = new SqlCommand("INSERT INTO MST_QuizWiseQuestions (QuizID, QuestionID, UserID, Created, Modified) VALUES (@QuizID, @QuestionID, @UserID, GETDATE(), GETDATE())", connection))
+                    {
+                        command.Parameters.AddWithValue("@QuizID", quizId);
+                        command.Parameters.AddWithValue("@QuestionID", questionId);
+                        command.Parameters.AddWithValue("@UserID", 1); // Replace with actual user ID
+                        command.ExecuteNonQuery();
+                    }
+                }
+            }
+        }
+
+        public int GetQuizIdByName(string quizName)
+        {
+            string connectionString = configuration.GetConnectionString("ConnectionString");
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                using (SqlCommand command = new SqlCommand("SELECT QuizID FROM MST_Quiz WHERE QuizName = @QuizName", connection))
+                {
+                    command.Parameters.AddWithValue("@QuizName", quizName);
+
+                    object result = command.ExecuteScalar();
+                    return result != null ? Convert.ToInt32(result) : 0; // Return 0 if not found
+                }
+            }
+        }
+
+
+        public List<int> GetAddedQuestionIds(int quizId)
+        {
+            List<int> questionIds = new List<int>();
+            string connectionString = configuration.GetConnectionString("ConnectionString");
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                using (SqlCommand command = new SqlCommand("SELECT QuestionID FROM MST_QuizWiseQuestions WHERE QuizID = @QuizID", connection))
+                {
+                    command.Parameters.AddWithValue("@QuizID", quizId);
+
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            questionIds.Add(reader.GetInt32(0));
+                        }
+                    }
+                }
+            }
+            return questionIds;
+        }
+
+        public bool AddQuizWiseQuestion(int quizId, int questionId, int userId)
+        {
+            string connectionString = configuration.GetConnectionString("ConnectionString");
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                using (SqlCommand command = new SqlCommand("PR_QuizQuestionsWise_Insert", connection))
+                {
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue("@QuizID", quizId);
+                    command.Parameters.AddWithValue("@QuestionID", questionId);
+                    command.Parameters.AddWithValue("@UserID", userId);
+                    command.Parameters.AddWithValue("@Created", DateTime.Now);
+                    command.Parameters.AddWithValue("@Modified", DateTime.Now);
+
+                    int result = command.ExecuteNonQuery();
+                    return result > 0;
+                }
+            }
+        }
+
+        public bool DeleteQuizWiseQuestion(int quizWiseQuestionId)
+        {
+            string connectionString = configuration.GetConnectionString("ConnectionString");
+
+            SqlConnection connection = new SqlConnection(connectionString);
+            connection.Open();
+            SqlCommand command = new SqlCommand("PR_QuizQuestionsWise_DeleteByPk", connection);
+            command.CommandType = CommandType.StoredProcedure;
+
+            command.Parameters.AddWithValue("@QuizWiseQuestionsID", quizWiseQuestionId);
+            int result = command.ExecuteNonQuery();
+            return result > 0;
+        }
+
 
     }
 }
